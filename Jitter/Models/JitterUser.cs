8f61d354f05d1eae239ccced451c360d4c7339af
Jeremy Grondahl <jeremygrondahl@gmail.com>
2015-11-19 12:10:35 -0600

    created class for jitter user

---
+++ Jitter/Models/JitterUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Jitter.Models
+{
+    public class JitterUser
+    {
+        public int JitterUserId { get; set; }
+        public string Description { get; set; }
+        public string FirstName { get; set; }
+        public string Handle { get; set; }
+        public string LastName { get; set; }
+        public string Picture { get; set; }
+    }
+}
