d10223811dd5e2ee15c4c217b1e24d5ea67f0892
Jeremy Grondahl <jeremygrondahl@gmail.com>
2015-11-20 12:26:53 -0600

    Created jitter repository and context classes.

---
+++ Jitter/Models/JitterContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Jitter.Models
+{
+    public class JitterContext : DbContext    
+    {
+        // IDbSet, IQueryable (Other options that developers may use in replace of DbSet)
+        public DbSet<JitterUser> JitterUsers { get; set; } // creates a jitter user table. 
+        public DbSet<Jot> Jots { get; set; }
+    }
+}
