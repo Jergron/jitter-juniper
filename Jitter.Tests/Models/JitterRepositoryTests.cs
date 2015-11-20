48470dfa493612987a13f67c1444f5ded8820923
Jeremy Grondahl <jeremygrondahl@gmail.com>
2015-11-20 12:24:30 -0600

    Created tests

---
+++ Jitter.Tests/Models/JitterRepositoryTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jitter.Models;
+using Moq;
+using System.Data.Entity;
+
+namespace Jitter.Tests.Models
+{
+    [TestClass]
+    public class JitterRepositoryTests
+    {
+        [TestMethod]
+        public void JitterContextEnsureICanCreateInstance()
+        {
+            JitterContext context = new JitterContext();
+            Assert.IsNotNull(context);
+        }
+
+        [TestMethod]
+        public void JitterRepositoryEnsureICanCreateInstance()
+        {
+            JitterContext repository= new JitterContext();
+            Assert.IsNotNull(repository);
+        }
+
+        [TestMethod]
+        public void JitterRepositoryEnsureICanGetAllUsers()
+        {
+            // Arrange
+            var expected = new List<JitterUser>
+            {
+                new JitterUser {Handle = "adam1" },
+                new JitterUser {Handle = "rumbadancer2" }
+            };
+            Mock<JitterContext> mock_context = new Mock<JitterContext>();
+
+            // <DbSet<JitterUser> in JitterContext.cs file
+            // Mocking the table we created.
+            Mock<DbSet<JitterUser>> mock_set = new Mock<DbSet<JitterUser>>();
+
+            mock_set.Object.AddRange(expected);
+
+            // This is Stubbing the JitterUsers property getter
+            // 'a' represents the instance of the jitter context
+            mock_context.Setup(a => a.JitterUsers).Returns(mock_set.Object);
+            JitterRepository repository = new JitterRepository(mock_context.Object);
+
+            // Act
+            var actual = repository.GetAllUsers();
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void JitterRepositoryEnsureIHaveAContext()
+        {
+            // Arrange
+            JitterRepository repository = new JitterRepository();
+            // Act
+            var actual = repository.Context;
+            // Assert
+            Assert.IsInstanceOfType(actual, typeof(JitterContext));
+        }
+        
+    }
+}
