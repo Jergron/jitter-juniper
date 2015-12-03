96cdd0fe4ec3ad59b38f990eb39e1d2a1817db29
Jeremy Grondahl <jeremygrondahl@gmail.com>
2015-12-03 12:07:36 -0600

    Created list of names to inject into view

--- Jitter/Controllers/HomeController.cs
+++ Jitter/Controllers/HomeController.cs
@@ -16,13 +16,19 @@ namespace Jitter.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            // 1. Create or get a list of things
+            List<string> my_list_of_things = new List<string>();
+            my_list_of_things.Add("Timmy");
+            my_list_of_things.Add("Greg");
+            my_list_of_things.Add("Chef");
 
-            return View();
+            return View(my_list_of_things);
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+            
 
             return View();
         }
